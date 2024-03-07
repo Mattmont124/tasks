4f1f6858af59257e57e9dd9487c32eb4362fd879
Matthew <mmontal@udel.edu>
2024-03-07 18:52:49 -0500

    Completed function EditMode/ corrected function

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, ChangeEvent } from "react";
 
 export function EditMode(): JSX.Element {
+    const [editMode, setEditMode] = useState(false);
+    const [userName, setUserName] = useState("Your Name");
+    const [isStudent, setIsStudent] = useState(true);
+
+    const handleSwitchChange = () => {
+        setEditMode((prevEditMode) => !prevEditMode);
+    };
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setUserName(e.target.value);
+    };
+
+    const handleCheckboxChange = () => {
+        setIsStudent((prevIsStudent) => !prevIsStudent);
+    };
     return (
         <div>
             <h3>Edit Mode</h3>
+            <div>
+                <label>
+                    Edit Mode:
+                    <input
+                        type="checkbox"
+                        checked={editMode}
+                        onChange={handleSwitchChange}
+                    />
+                </label>
+            </div>
+            {editMode ? (
+                <div>
+                    <label>
+                        Your Name:
+                        <input
+                            type="text"
+                            value={userName}
+                            onChange={handleNameChange}
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={isStudent}
+                            onChange={handleCheckboxChange}
+                        />
+                        Are you a student?
+                    </label>
+                </div>
+            ) : (
+                <div>
+                    {userName} is {isStudent ? "a student" : "not a student"}
+                </div>
+            )}
         </div>
     );
 }
