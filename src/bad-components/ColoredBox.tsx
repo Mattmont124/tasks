b125ff5e5c697e2652d22e134d4907f553cff94e
Matthew <mmontal@udel.edu>
2024-03-06 19:52:00 -0500

    Completed ColoredBox function/ corrected it

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -6,21 +6,20 @@ const DEFAULT_COLOR_INDEX = 0;
 
 function ChangeColor(): JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
-    );
+    const nextColor = () => {
+        setColorIndex((colorIndex + 1) % COLORS.length);
+    };
+    return <Button onClick={nextColor}>Next Color</Button>;
 }
 
-function ColorPreview(): JSX.Element {
+function ColorPreview({ color }: { color: string }): JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: color,
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
@@ -30,13 +29,14 @@ function ColorPreview(): JSX.Element {
 }
 
 export function ColoredBox(): JSX.Element {
+    const [colorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
                 <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ColorPreview color={COLORS[colorIndex]}></ColorPreview>
             </div>
         </div>
     );
