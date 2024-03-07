ec739bda16303ba3e46152a12e86d40ba9042420
Matthew <mmontal@udel.edu>
2024-03-07 18:29:08 -0500

    Completed function CheckAnswer/corrected it

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,16 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserAnswer(e.target.value);
+    };
     return (
         <div>
             <h3>Check Answer</h3>
+            <input type="text" value={userAnswer} onChange={handleChange} />
+            {userAnswer === expectedAnswer ? <span>✔️</span> : <span>❌</span>}
         </div>
     );
 }
